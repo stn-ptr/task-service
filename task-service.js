62bfcff9e2b4169256f2ecf339d4e1909fe8d58a
stn_ptr <34418806+stn-ptr@users.noreply.github.com>
2019-04-06 21:03:15 +0200

    extract username and password from request

--- task-service.js
+++ task-service.js
@@ -3,6 +3,22 @@
 var http = require('http');
 
 http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' })
-        res.end('Node.js server is running');
-    }).listen(1337);
+	
+	var header=req.headers['authorization']||'';
+	
+	if (header.length === 0) {
+		res.statusCode = 401
+		res.setHeader('WWW-Authenticate', 'Basic realm="task-service"');
+		res.end('Access denied');
+	} else {
+
+   		var	token=header.split(/\s+/).pop()||'',            // and the encoded auth token
+      	auth=new Buffer.from(token, 'base64').toString(),    // convert from base64
+      	parts=auth.split(/:/),                          // split on colon
+      	username=parts[0],
+      	password=parts[1];
+
+		res.writeHead(200, { 'Content-Type': 'text/plain' })
+   		res.end('Username: ' + username + '; Password: '+ password);
+   	}
+}).listen(1337);
