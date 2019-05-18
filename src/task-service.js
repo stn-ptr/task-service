bfbd61de36916139c916deb6179fbb14bb1b432b
stn_ptr <34418806+stn-ptr@users.noreply.github.com>
2019-05-18 12:07:10 +0200

    removed "from" from buffer (not available innode  target version)

--- src/task-service.js
+++ src/task-service.js
@@ -32,18 +32,17 @@ https.createServer(httpOptions, function (req, res) {
 	var header=req.headers['authorization']||'';
 	
 	if (header.length === 0) {
-		res.statusCode = 401
+		res.statusCode = 401;
 		res.setHeader('WWW-Authenticate', 'Basic realm="task-service"');
 		res.end('Access denied');
 	} else {
+		var	token = header.split(/\s+/).pop() || '';
+		var auth = new Buffer(token, 'base64').toString();
+		var parts = auth.split(/:/);
+		var username = parts[0];
+		var password = parts[1];
 
-   		var	token=header.split(/\s+/).pop()||'',            // and the encoded auth token
-      	auth=new Buffer.from(token, 'base64').toString(),    // convert from base64
-      	parts=auth.split(/:/),                          // split on colon
-      	username=parts[0],
-      	password=parts[1];
-
-		res.writeHead(200, { 'Content-Type': 'text/plain' })
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
    		res.end('Username: ' + username + '; Password: '+ password);
    	}
 }).listen(1337);
