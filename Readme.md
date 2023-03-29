3b41fe19d916466d0d7f9bcca5e76af92958f716
stn_ptr <34418806+stn-ptr@users.noreply.github.com>
2023-03-29 21:56:42 +0200

    reformat files with Prettier

--- Readme.md
+++ Readme.md
@@ -1,9 +1,12 @@
 # Task Service
-Not fit for production use. This is a project for training and trying out things, 
+
+Not fit for production use. This is a project for training and trying out things,
 please do not use in a productive environment.
 
 ## Requirements
-* Node 0.10.48 - Ancient, but this version runs on my NAS, which is ancient as well.
+
+- Node 0.10.48 - Ancient, but this version runs on my NAS, which is ancient as well.
 
 ## Features
+
 Supports HTTPS
--- server.js
+++ server.js
@@ -1,60 +1,61 @@
-'use strict';
+"use strict";
 
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
 
-const configFileOptions = {encoding: 'utf8'};
+const configFileOptions = { encoding: "utf8" };
 
 const configFile = getConfigFile(process.argv);
 
 let config = undefined;
 
 if (configFile) {
-    fs.exists(configFile, function(exists) {
-        if (!exists) {
-            process.exit(1);
-        }
-    });
+  fs.exists(configFile, function (exists) {
+    if (!exists) {
+      process.exit(1);
+    }
+  });
 
-	const fileContents = fs.readFileSync(configFile, configFileOptions);
-	config = JSON.parse(fileContents);
+  const fileContents = fs.readFileSync(configFile, configFileOptions);
+  config = JSON.parse(fileContents);
 } else {
-    process.exit(1);
+  process.exit(1);
 }
 
 const httpOptions = {
-	key: fs.readFileSync(config.HttpsOptions.key),
-	cert: fs.readFileSync(config.HttpsOptions.cert)
+  key: fs.readFileSync(config.HttpsOptions.key),
+  cert: fs.readFileSync(config.HttpsOptions.cert),
 };
 
-https.createServer(httpOptions, function (req, res) {
+https
+  .createServer(httpOptions, function (req, res) {
+    const header = req.headers["authorization"] || "";
 
-	const header = req.headers['authorization'] || '';
+    if (header.length === 0) {
+      res.statusCode = 401;
+      res.setHeader("WWW-Authenticate", 'Basic realm="task-service"');
+      res.end("Access denied");
+    } else {
+      const token = header.split(/\s+/).pop() || "";
+      const auth = new Buffer(token, "base64").toString();
+      const parts = auth.split(/:/);
+      const username = parts[0];
+      const password = parts[1];
 
-	if (header.length === 0) {
-		res.statusCode = 401;
-		res.setHeader('WWW-Authenticate', 'Basic realm="task-service"');
-		res.end('Access denied');
-	} else {
-		const token = header.split(/\s+/).pop() || '';
-		const auth = new Buffer(token, 'base64').toString();
-		const parts = auth.split(/:/);
-		const username = parts[0];
-		const password = parts[1];
-
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
-   		res.end('Username: ' + username + '; Password: '+ password);
-   	}
-}).listen(1337);
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Username: " + username + "; Password: " + password);
+    }
+  })
+  .listen(1337);
 
 function getConfigFile(args) {
-
-	const configParam = '/--ConfigurationFile=/';
-	let configFile = undefined;
-	args.forEach(function(arg) {
-		configFile = arg.search(configParam) >= 0
-		? arg.slice(configParam.length - 2) 
-		: configFile;
-	});
-	return configFile;
+  const configParam = "/--ConfigurationFile=/";
+  let configFile = undefined;
+  args.forEach(function (arg) {
+    configFile =
+      arg.search(configParam) >= 0
+        ? arg.slice(configParam.length - 2)
+        : configFile;
+  });
+  return configFile;
 }
