b75a403ab6841064ed2310ac71a4ff3ebaeb3980
stn_ptr <34418806+stn-ptr@users.noreply.github.com>
2025-07-18 21:36:25 +0200

    add task creation endpoint

--- index.js
+++ index.js
@@ -57,9 +57,39 @@ server
       const parts = auth.split(/:/);
       const username = parts[0];
       const password = parts[1];
+      const url = require('node:url')
+      const parsedUrl = url.parse(req.url, true);
+      const method = req.method;
+      const pathname = parsedUrl.pathname;
 
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Username: " + username + "; Password: " + password);
+      // CORS Headers für Frontend-Zugriff
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+      // POST /tasks - Neuen Task erstellen
+      if (method === 'POST' && pathname === '/task') {
+        let body = '';
+        
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        
+        req.on('end', () => {
+          try {
+            const requestBody = JSON.parse(body);
+            const task = require('./task/task.js');
+            const responseBody = task.create(requestBody.title)
+
+            res.writeHead(201, { "Content-Type": "text/plain" });
+            res.end(JSON.stringify(responseBody));
+          }
+          catch (e) {
+            res.writeHead(500, {'Content-Type': 'application/json'})
+            res.end(JSON.stringify({'error': 'error creating the task' }))
+          }
+        })
+      }
     }
   })
   .listen(1337);
