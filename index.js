987e57dcc1b86cced508635e1dc4a899027fff8d
stn_ptr <34418806+stn-ptr@users.noreply.github.com>
2025-07-19 10:10:37 +0200

    move app logic into app/app.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const { getConfig } = require("./app/config.js");
+const { app } = require("./app/app.js");
 
 const config = getConfig();
 
@@ -27,52 +28,4 @@ if (httpOptions.key && httpOptions.cert) {
   server = require("node:http");
 }
 
-server
-  .createServer(httpOptions, function (req, res) {
-    const header = req.headers["authorization"] || "";
-
-    if (header.length === 0) {
-      res.statusCode = 401;
-      res.setHeader("WWW-Authenticate", 'Basic realm="task-service"');
-      res.end("Access denied");
-    } else {
-      const token = header.split(/\s+/).pop() || "";
-      const auth = new Buffer(token, "base64").toString();
-      const parts = auth.split(/:/);
-      const username = parts[0];
-      const password = parts[1];
-      const url = require("node:url");
-      const parsedUrl = url.parse(req.url, true);
-      const method = req.method;
-      const pathname = parsedUrl.pathname;
-
-      // CORS Headers für Frontend-Zugriff
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-      // POST /tasks - Neuen Task erstellen
-      if (method === "POST" && pathname === "/task") {
-        let body = "";
-
-        req.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-
-        req.on("end", () => {
-          try {
-            const requestBody = JSON.parse(body);
-            const task = require("./task/task.js");
-            const responseBody = task.create(requestBody.title);
-
-            res.writeHead(201, { "Content-Type": "text/plain" });
-            res.end(JSON.stringify(responseBody));
-          } catch (e) {
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "error creating the task" }));
-          }
-        });
-      }
-    }
-  })
-  .listen(1337);
+server.createServer(httpOptions, app).listen(1337);
